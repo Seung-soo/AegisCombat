d03fc6ea085eeb9dd837e321a29e6a3127409346
Seungsoo Kim <kin2398@naver.com>
2026-02-13 02:14:28 +0900

    chore: initial blank UE project

---
+++ Source/AegisCombat/Private/Core/AegisLog.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AegisCombat/Public/Core/AegisLog.h"
+
+DEFINE_LOG_CATEGORY(LogAegis);
+DEFINE_LOG_CATEGORY(LogAegisCombat);
+DEFINE_LOG_CATEGORY(LogAegisAnim);
